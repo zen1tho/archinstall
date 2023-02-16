1d495b50df1061ad3c1dc82f71cb7ebc660e6374
zen1tho <zen1tho.codes@gmail.com>
2023-02-17 00:37:14 +1100

    start refactoring demo code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use std::error::Error;
+mod app;
 mod init;
+mod ui;
 
-pub fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, World!");
-    init::init()
+use argh::FromArgs;
+use std::{error::Error, time::Duration};
+
+/// Demo
+#[derive(Debug, FromArgs)]
+struct Cli {
+    /// time in ms between two ticks.
+    #[argh(option, default = "250")]
+    tick_rate: u64,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli: Cli = argh::from_env();
+    let tick_rate = Duration::from_millis(cli.tick_rate);
+    init::run(tick_rate, true)?;
+    Ok(())
 }
